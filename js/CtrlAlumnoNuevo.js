2644197351d969cbf3cc8d5e56a50330d0a93067
Alumnos41 <82115443+Alumnos41@users.noreply.github.com>
2021-04-07 14:17:46 -0500

    Update CtrlAlumnoNuevo.js

--- js/CtrlAlumnoNuevo.js
+++ js/CtrlAlumnoNuevo.js
@@ -16,8 +16,13 @@ import {
 const daoAlumno =
   getFirestore().
     collection("Alumno");
+const params =
+  new URL(location.href).
+    searchParams;
+const id = params.get("id");
 /** @type {HTMLFormElement} */
 const forma = document["forma"];
+
 getAuth().onAuthStateChanged(
   protege, muestraError);
 
@@ -27,8 +32,41 @@ getAuth().onAuthStateChanged(
 async function protege(usuario) {
   if (tieneRol(usuario,
     ["Administrador"])) {
-    forma.addEventListener(
-      "submit", guarda);
+    busca();
+  }
+}
+
+/** Busca y muestra los datos que
+ * corresponden al id recibido. */
+async function busca() {
+  try {
+    const doc =
+      await daoAlumno.
+        doc(id).
+        get();
+    if (doc.exists) {
+      /**
+       * @type {
+          import("./tipos.js").
+                  Alumno} */
+      const data = doc.data();
+      forma.matricula.value = data.matricula;
+      forma.nombre.value = data.nombre || "";
+      forma.telefono.value = data.telefono || "";
+      forma.grupo.value = data.grupo || "";
+      forma.fecha.value = data.fecha || "";
+      forma.addEventListener(
+        "submit", guarda);
+      forma.eliminar.
+        addEventListener(
+          "click", elimina);
+    } else {
+      throw new Error(
+        "No se encontró.");
+    }
+  } catch (e) {
+    muestraError(e);
+    muestraAlumnos();
   }
 }
 
@@ -40,19 +78,39 @@ async function guarda(evt) {
       new FormData(forma);
     const matricula = getString(
         formData, "matricula").trim();  
-    const nombre = getString(
-      formData, "nombre").trim();
+    const nombre = getString(formData, "nombre").trim();
+    const telefono = getString(formData, "telefono").trim();
+    const grupo = getString(formData, "grupo").trim();
+    const fecha = getString(formData, "fecha").trim();
     /**
      * @type {
         import("./tipos.js").
                 Alumno} */
     const modelo = {
-      matricula,
-      nombre
+      matricula, 
+      nombre,
+      telefono,
+      grupo,
+      fecha
     };
     await daoAlumno.
-      add(modelo);
+      doc(id).
+      set(modelo);
     muestraAlumnos();
+  } catch (e) {
+    muestraError(e);
+  }
+}
+
+async function elimina() {
+  try {
+    if (confirm("Confirmar la " +
+      "eliminación")) {
+      await daoAlumno.
+        doc(id).
+        delete();
+      muestraAlumnos();
+    }
   } catch (e) {
     muestraError(e);
   }
